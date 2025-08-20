341128d0e05a5907193a7a98c6a4d9e6d2ef2df4
neelk <neelesh.karthi@gmail.com>
2025-08-20 13:28:13 -0700

    refactor: remove unnecessary form info section from TestSchedules component for cleaner UI

--- client/src/components/TestSchedules.jsx
+++ client/src/components/TestSchedules.jsx
@@ -135,14 +135,6 @@ function TestSchedules() {
               </button>
             </div>
             <div className="modal-body">
-              <div className="form-info">
-                <span className="info-icon">
-                  <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-                    <path d="M19 3h-1V1h-2v2H8V1H6v2H5c-1.11 0-1.99.9-1.99 2L3 19c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V8h14v11zM7 10h5v5H7z"/>
-                  </svg>
-                </span>
-                <span className="info-text">Set up automated testing for your prompt systems</span>
-              </div>
               <form onSubmit={handleSubmit} className="schedule-form">
             <div className="form-group">
               <label>Prompt System:</label>
